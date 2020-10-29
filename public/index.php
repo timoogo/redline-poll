733d59be866218e8ef7b7f401bd05c58c85f74f5
= <=>
2020-10-29 23:29:01 +0100

    :art: nav + hero > centered done

--- public/index.php
+++ public/index.php
@@ -6,23 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Redline - un nième site de sondage </title>
     <link rel="stylesheet" href="../public/assets/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 
 <body>
-  <header>
-      <a href="index.php" class="logo">
-          <img src="assets/img/logo.png" alt="logo">
-      </a>
-      <nav>
-          <ul>
-              <li><a href="#">item</a></li>
-              <li><a href="#">item</a></li>
-              <li><a href="#">item</a></li>
-          </ul>
-      </nav>
-  </header>
-    
+    <header>
+        <a href="index.php" class="logo">
+            <img src="assets/img/logo.jpg" alt="logo">
+        </a>
+        <nav>
+            <ul>
+                <li><a href="#">item</a></li>
+                <li><a href="#">item</a></li>
+                <li><a href="#">item</a></li>
+            </ul>
+        </nav>
+        <ul class="action">
+            <li>
+                <a href="#"> Connexion </a>
+                <a href="#"> Inscription </a>
+            </li>
+        </ul>
+    </header>
+
+    <section class="hero">
+        <div class="centered">
+            <h1>Participez à des <br> sondages en <span class="highlight">temps réel </span>  et comparez vos résultats</h1>
+            <a href="#">Regardez un exemple
+                <i class="fas fa-angle-double-down"></i>
+            </a>
+        </div>
+    </section>
+    <section class="display-an-example">
+        <!-- <img class="background" src="assets/img/bg.png"></img> -->
+        <div class="forground">
+           <div class="title">
+            <h2>Les céréales, <span class="highlight">avant</span> ou <span class="highlight">après</span> le lait </h2>
+            <h3>Question posée par <a href="#" class=" highlight">timogo</a></h3>
+           </div>
+           <div class="flex-hero">
+            
+           </div>
+        </div>
+    </section>
 </body>
 
 </html>
